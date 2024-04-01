003599e9befa0a5b97e16fd9e2b7ceed7a88f95a
sasi-hemanth <143342032+sasi-hemanth@users.noreply.github.com>
2024-03-31 20:28:04 -0400

    Add files via upload

---
+++ To Do Assignment/To Do AssignmentUITests/To_Do_AssignmentUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  To_Do_AssignmentUITestsLaunchTests.swift
+//  To Do AssignmentUITests
+//
+//  Created by Sasi Hemanth Siripurapu on 2024-03-31.
+//
+
+import XCTest
+
+final class To_Do_AssignmentUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
